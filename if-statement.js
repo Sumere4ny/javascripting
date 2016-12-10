6d22e380cd769db9cfdcc37f41c2b671d41e8ec7
Pavel Kiselev <sumere4ny@gmail.com>
2016-12-10 21:15:33 +0300

    Next four tasks

---
+++ if-statement.js
@@ -0,0 +1,9 @@
+var fruit = 'banana';
+var result;
+if (fruit.length > 5) {
+  result = 'more than five characters.';
+} else {
+  result = 'five characters or less.';
+}
+
+console.log('The fruit name has ' + result);
